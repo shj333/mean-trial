3e55dbd3fa78f823378a7a205c79251736dc3e90
stuart <stuartj@berwickheights.com>
2015-02-15 16:48:25 -0500

    Refactor for MVC

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var mainCtrl = require('../server/controllers/main');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  mainCtrl.index(req, res, next);
 });
 
 module.exports = router;
